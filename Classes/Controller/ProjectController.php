7e850422fedbfee853b01a58fab76a684bfb6dae
Jessica Schlierenkamp <mail@schlierenkamp.de>
2020-04-17 21:19:55 +0200

    [TASK] add filter flexform for projects plugin
    Signed-off-by: Jessica Schlierenkamp <mail@schlierenkamp.de>

--- Classes/Controller/ProjectController.php
+++ Classes/Controller/ProjectController.php
@@ -9,12 +9,37 @@ declare(strict_types = 1);
 
 namespace JS\Wechange\Controller;
 
+use JS\Wechange\Domain\Model\Filter\ProjectFilter;
+use JS\Wechange\Domain\Repository\ProjectRepository;
+use JS\Wechange\Service\ApiService;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class ProjectController extends ActionController
 {
+    /**
+     * @var ProjectRepository
+     */
+    private ProjectRepository $projectRepository;
+
+    public function initializeAction(): void
+    {
+        $this->projectRepository = new ProjectRepository($this->settings['api']['baseUrl'], new ApiService());
+    }
+
     public function listAction(): void
     {
-        $this->view->assign('projects', []);
+        $projectFilter = new ProjectFilter(
+            (int)$this->settings['filter']['parent'],
+            $this->settings['filter']['tag'],
+            (int)$this->settings['filter']['limit'],
+            $this->settings['filter']['orderBy'],
+            $this->settings['filter']['orderDir']
+        );
+
+        try {
+            $this->view->assign('projects', $this->projectRepository->findForFilter($projectFilter));
+        } catch (\JsonException $e) {
+            $this->view->assign('projects', []);
+        }
     }
 }
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,3 +7,5 @@ defined('TYPO3_MODE') or die();
     'LLL:EXT:wechange/Resources/Private/Language/locallang_db.xlf:plugin.projects'
 );
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['wechange_projects'] = 'recursive,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['wechange_projects'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('wechange_projects', 'FILE:EXT:wechange/Configuration/FlexForms/flexform_projects.xml');
