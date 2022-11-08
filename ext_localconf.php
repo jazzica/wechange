d4bb2e7940504b3406ca6f95e461f9f01df3e5a0
Jessica Schlierenkamp <mail@schlierenkamp.de>
2022-11-08 15:05:15 +0100

    [TASK] configure ce wizard for plugins

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,46 +19,19 @@ call_user_func(
             ]
         );
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            'mod {
-                wizards.newContentElement.wizardItems.plugins {
-                    elements {
-                        projects {
-                            iconIdentifier = wechange-plugin-projects
-                            title = LLL:EXT:wechange/Resources/Private/Language/locallang_db.xlf:plugin.projects
-                            description = LLL:EXT:wechange/Resources/Private/Language/locallang_db.xlf:plugin.projects.description
-                            tt_content_defValues {
-                                CType = list
-                                list_type = wechange_projects
-                            }
-                        }
-                        map {
-                            iconIdentifier = wechange-plugin-map
-                            title = LLL:EXT:wechange/Resources/Private/Language/locallang_db.xlf:plugin.map
-                            description = LLL:EXT:wechange/Resources/Private/Language/locallang_db.xlf:plugin.map.description
-                            tt_content_defValues {
-                                CType = list
-                                list_type = wechange_map
-                            }
-                        }
-                    }
-                    show = *
-                }
-           }'
-        );
-
         /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
             \TYPO3\CMS\Core\Imaging\IconRegistry::class
         );
+        $iconRegistry->registerIcon(
+            'wechange-plugin',
+            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+            ['source' => 'EXT:wechange/Resources/Public/Icons/wechange.png']
+        );
 
-        foreach (['projects', 'map'] as $plugin) {
-            $iconRegistry->registerIcon(
-                'wechange-plugin-' . $plugin,
-                \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-                ['source' => 'EXT:wechange/Resources/Public/Icons/wechange.png']
-            );
-        }
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:wechange/Configuration/TsConfig/All.tsconfig">'
+        );
 
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['wechange_projectList'] ??= [];
     }
