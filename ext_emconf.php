63e71b6ba5407f9fa917691555a704711f26a136
Jessica Schlierenkamp <mail@schlierenkamp.de>
2022-11-08 10:55:33 +0100

    [TASK] remove TYPO3 9.5 from dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@schlierenkamp.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.9-10.4.99',
-            'php' => '7.4.0-7.4.99'
+            'typo3' => '10.4.0-11.5.99'
         ],
         'conflicts' => [
         ],
